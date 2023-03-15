3330ecf7c900d5c316e1336dd2ecafa082ec2aaa
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2023-03-16 00:08:17 +0500

    Update tailwind configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,30 @@ module.exports = {
   },
   plugins: [
     require('flowbite/plugin')
-  ]
+  ],
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+      borderColor: ['dark'],
+      divideColor: ['dark'], // enable the dark variant for dividing line colors
+      boxShadow: ['dark'], // enable the dark variant for box shadows
+      placeholderColor: ['dark'], // enable the dark variant for placeholder text colors
+      ringColor: ['dark'], // enable the dark variant for ring colors
+      ringOffsetColor: ['dark'], // enable the dark variant for ring offset colors
+      ringOffsetWidth: ['dark'], // enable the dark variant for ring offset widths
+      ringOpacity: ['dark'], // enable the dark variant for ring opacity
+      ringWidth: ['dark'], // enable the dark variant for ring widths
+      fill: ['hover', 'focus'], // enable hover and focus variants for SVG fill colors
+      stroke: ['hover', 'focus'], // enable hover and focus variants for SVG stroke colors
+      gradientColorStops: ['hover', 'focus'], // enable hover and focus variants for gradient color stops
+      zIndex: ['hover', 'active'], // enable hover and active variants for z-index values
+      scale: ['hover', 'active'], // enable hover and active variants for scale values
+      rotate: ['hover', 'active'], // enable hover and active variants for rotation values
+      translate: ['hover', 'active'],
+      skew: ['hover', 'active'],
+      transitionProperty: ['hover', 'focus'],
+      outline: ['focus'],
+    }
+  },
 }
